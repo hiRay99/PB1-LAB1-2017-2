8461312fabfe5a7fb548139090d10857288f481b
Rayane Ribeiro <rayaneribeiro99@hotmail.com>
2017-09-24 19:57:02 -0400

    3

---
+++ WebAppProvaa/Models/Veiculo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAppProvaa.Models
+{
+    public class Veiculo
+    {
+        public Boolean Disponivel { get; set; }
+        public Boolean Indisponivel { get; set; }
+        public DateTime DataCadastro { get; set; }
+    }
+}
